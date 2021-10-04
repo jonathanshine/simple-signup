42683efd5a67885ab6a64d4efb2e5c47e3d3c958
Jonathan Shine <jonathanhshine@gmail.com>
2021-10-04 11:39:58 +0200

    added first route and listener to localhost at port 5000

--- signup-api/server.js
+++ signup-api/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import cors from "cors";
 // --------------------------------------------------
 
 
@@ -24,3 +25,25 @@ mongoose.connect(MONGO_URI, {
 .then(() => console.log("Successfully connected to the database"))
 .catch((error) => console.log("Unable to establish connection to database --> ", error));
 // --------------------------------------------------
+
+
+// MIDDLEWARE ---------------------------------------
+app.use( express.json() );
+
+app.use( cors() );
+// --------------------------------------------------
+
+
+// ROUTES -------------------------------------------
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to the Simple-Signup API</h1>");
+});
+
+
+// --------------------------------------------------
+
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
